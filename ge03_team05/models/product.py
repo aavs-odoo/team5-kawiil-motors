231b9167304fefd7e48b6bc505c01b7dd4874507
Ignacio Moral (IGCM) <igcm@odoo.com>
2023-07-24 17:31:51 -0600

    Prueba ge06

---
+++ ge03_team05/models/product.py
@@ -0,0 +1,15 @@
+from odoo import api, fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.onchange('model', 'make', 'year')
+    def _generate_name(self):
+        product_registries = self.filtered(lambda r: r.detailed_type == 'motorcycle')
+        for product in product_registries:
+            if product.make == False:
+                product.make = '--'
+            if product.model == False:
+                product.model = '--' 
+            product.name = str(product.make) + str(product.model) + str(product.year)
