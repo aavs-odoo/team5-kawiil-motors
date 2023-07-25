10c0e217d56f6dcb91810fed120760ff20bc3f05
dihg-odoo <dihg@odoo.com>
2023-07-25 11:27:26 -0600

    ge07 Finalizado

---
+++ ge06_team05/__manifest__.py
@@ -0,0 +1,34 @@
+{
+    "name": "Motorcycle Serial Number Generator",
+    
+    "summary": "Automatically generates VIN and Serial Number of a Motorcycle with it's Sale Order.",
+    
+    "description": """
+    Motorcycle Serial Number Generator
+====================
+    This Module automatically generates the lot serial number depending on its VIN.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["stock", "mrp", "motorcycle_registry", "sale",],
+    
+    "data": [
+    ],
+    
+    "demo": [
+        "demo/motorcycle_registry_demo.xml",
+        "demo/product_demo.xml",
+    ],
+    
+    "author": "kauil-motors",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+    
+}
