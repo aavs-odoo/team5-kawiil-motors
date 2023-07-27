fcb846629ca159470cc6b01cc15bc68fa6a3b437
Ignacio Moral (IGCM) <igcm@odoo.com>
2023-07-27 17:25:19 -0600

    Generated the Module

---
+++ ge09_team05/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "ge09_team05",
+    
+    "summary": "Help Kawiil Motorcycles Manage Repair Orders",
+    
+    "description": """GE09_Team05 Motorcyle Repair Enhancements""",
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["repair", "ge07_team05"],
+    
+    "data": [
+        'views/motorcycle_registry_form_inherit.xml',
+        'views/repair_order_inherit.xml',
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "Ignacio Moral (IGCM) - Team 05",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+}
