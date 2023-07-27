338c14e2c9ef1dde2002c94ca29fed4390e0e2fd
aavs-odoo <aavs@odoo.com>
2023-07-27 11:38:25 -0600

    ge04 terminado

--- ge04_team05/models/sale_order.py
+++ ge04_team05/models/sale_order.py
@@ -1,5 +1,13 @@
 from odoo import fields, models
 
+
 class SaleOrder(models.Model):
-    _inherit="sale.order"
-    customer_type = fields.Boolean(related="partner_id.is_new_customer")
+
+    _inherit = "sale.order"
+
+    is_new_customer = fields.Boolean(related="partner_id.is_new_customer")
+
+    def setpricelist(self):
+        for order in self:
+            order.pricelist_id = 3
+            order.action_update_prices()
