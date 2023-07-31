2a8ec18f44ab7c2945f8203e4c6b2ca73508d31c
Ignacio Moral (IGCM) <igcm@odoo.com>
2023-07-31 17:58:37 -0600

    [ADD] Generated Module for Automatic Repair Orders, generated Create and Search Order Functionalities, generated Breadcrumbs and Home Menu Item
    K'awiil has asked us to generate an addition to the portal. They have expanded, and their business includes repairing the motorcycles they sell. This module helps them generate automatically new Repair Orders from the portal.
    
    id: 3426548
    Signed-off-by: Ignacio Joaquin Moral (igcm) <igcm@odoo.com>

---
+++ ge11_team05/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name": "ge11_team05",
+    
+    "summary": "Help K\'awiil Motorcycles manage its portal to allow maintenance requests.",
+    
+    "description": """GE11_Team05 Portal Mainteinance Requests""",
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["repair", "ge09_team05", "portal"],
+    
+    "data": [
+        "views/repair_portal_templates.xml"
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "Ignacio Moral (IGCM) - Team 05",
+    
+    "website": "www.odoo.com",
+    
+    "application": True,
+}
